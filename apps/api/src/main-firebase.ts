61cc79c572d6a40b1fb5563de97002ecd0599476
hritambh <hritambh9@gmail.com>
2025-12-31 08:20:41 +0530

    new changes

--- apps/api/src/main-firebase.ts
+++ apps/api/src/main-firebase.ts
@@ -3,6 +3,7 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
 import express from 'express';
 import { onRequest } from 'firebase-functions/v2/https';
+import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
 const expressServer = express();
 
@@ -10,7 +11,18 @@ const createFunction = async (expressInstance) => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
+    { 
+      bodyParser: false 
+    }
   );
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+  app.use((req, res, next) => {
+    if (req.path.includes('/menu/scan')) {
+      console.log(`[DEBUG] POST /scan hit. Content-Type: ${req.headers['content-type']}`);
+    }
+    next();
+  });
   
   app.setGlobalPrefix('api');
   
